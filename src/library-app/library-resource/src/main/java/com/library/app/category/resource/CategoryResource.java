692df6cb6f06435539f9a8700777d7e3af4cb930
Michael Davidson <michael.davidson@devinformatics.com>
2015-08-04 23:18:06 -0500

    Completed lecture 22.

---
+++ src/library-app/library-resource/src/main/java/com/library/app/category/resource/CategoryResource.java
@@ -0,0 +1,15 @@
+package com.library.app.category.resource;
+
+import javax.ws.rs.core.Response;
+
+import com.library.app.category.services.CategoryServices;
+
+public class CategoryResource {
+
+	CategoryServices categoryService;
+
+	public Response add(final String readJsonFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
